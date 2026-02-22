013b21c0b358f401104db23fcd6c4113242d4f09
Wassim MNASSER <mnasserwassim@gmail.com>
2026-02-22 22:23:36 +0100

    Initial Spring Boot project

---
+++ src/main/java/com/foobarquix/FoobarQuixApplication.java
@@ -0,0 +1,13 @@
+package com.foobarquix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoobarQuixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoobarQuixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/foobarquix/FoobarQuixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foobarquix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoobarQuixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
